21685a2d3eaecc6fcde578362c1ff07a110cb689
Angel_Fora <a1122150052@utch.edu.mx>
2024-09-05 16:54:33 -0600

    pruebaimg2

--- src/app/components/collections.tsx
+++ src/app/components/collections.tsx
@@ -25,7 +25,7 @@ export default function FeaturedCategories() {
     },
     {
       name: "Collares",
-      image: "/Collar.jpg",
+      image: "/images/Collar.jpg",
       description:
         "Nuestros collares combinan elegancia y tendencia para iluminar tu look con un toque de distinción.",
     },
