7564a3432e4e08e421a69ef7403cf4dd4acd79d7
Angel_Fora <a1122150052@utch.edu.mx>
2024-09-05 16:38:58 -0600

    nombre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,32 @@ import FeaturedCategories from "./components/collections";
 import FeaturesSection from "./components/featuresSection";
 import CtaSection from "./components/ctaSection";
 import Footer from "./components/footer";
-import { Suspense } from "react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <main>
+      <head>
+        <title>Luxe</title>
+        <meta
+          name="description"
+          content="Descubre nuestra colección exclusiva de joyería de lujo: anillos, collares y pendientes diseñados para resaltar tu estilo y elegancia."
+        />
+        <meta
+          name="keywords"
+          content="joyería, anillos, collares, pendientes, lujo, exclusividad"
+        />
+        <meta name="author" content="Tu Nombre o Empresa" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
         <Nav />
         <HeroSection />
         <FeaturedCategories />
         <FeaturesSection />
         <CtaSection />
         <Footer />
-      </main>
+      </body>
     </>
   );
 }
